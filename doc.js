91c100c89f4d9e0f3ef74e102060799f1d4c4093
Daniel <akirazcs@gmail.com>
2026-04-16 10:43:52 -0300

    fix de alguns erros + implementação/otimização de funções

--- doc.js
+++ doc.js
@@ -1,7 +1,45 @@
 let cod = localStorage.getItem("codigoProcesso");
 let nome = localStorage.getItem("nomeRequerente");
-let municipio = localStorage.getItem("nomeMunicipio")
+let req = localStorage.getItem("requerimento")
+let ato = localStorage.getItem("ato")
+let atvd = localStorage.getItem("atividade")
+let porte = localStorage.getItem("porte")
+let areaProp = localStorage.getItem("Prop")
+let areaAtvd = localStorage.getItem("areaAtvd")
+let cid = localStorage.getItem("nomeMunicipio")
+let endereco = localStorage.getItem("Endereço")
 
-document.getElementById("mostrarCod").textContent = cod;
-document.getElementById("mostrarNome").textContent = nome;
-document.getElementById("mostrarCid").textContent = municipio;
+const mostrar = (id, valor) => { //função pra pegar todos os elementos que vai ter o id ali, e substituir o texto por valor, e a lista retornar 0 no lugar ele coloca ??
+    const elementos = document.querySelectorAll(`[data-campo='${id}']`);
+
+    if (elementos.length === 0) return; 
+
+    elementos.forEach(elemento => {
+        elemento.textContent = valor ?? "";
+    });
+};
+
+
+mostrar("cod", cod);
+mostrar("req", req);
+mostrar("nome", nome);
+mostrar("ato", ato);
+mostrar("porte", porte);
+mostrar("atvd", atvd);
+mostrar("areaProp", areaProp);
+mostrar("areaAtvd", areaAtvd);
+mostrar("cid", cid)
+mostrar("endereco", endereco)
+const data  = () => {
+    const hoje = new Date()
+    let dia = hoje.getDate();
+    let mes = hoje.toLocaleString('pt-BR', {month: 'long'});
+    let ano = hoje.getFullYear();
+
+    return "Palmas, " + dia +" de " + mes + " de " + ano;
+}
+
+console.log(data());
+
+let calendario = data();
+mostrar("calendario", calendario)
--- form.js
+++ form.js
@@ -3,15 +3,31 @@ const form = document.querySelector("form");
 form.addEventListener("submit", function(event){ // praticamente quando o formulario for enviado vai executar a função
     event.preventDefault(); // impede a paginad e recarregar e perder o processo
 
+    const salvar = (chave, valor) => localStorage.setItem(chave, valor)
+
     let cod = document.getElementById("cod_processo").value;
     let nome = document.getElementById("nome_requerente").value;
     // let estado = document.getElementById("estado").value;
     let municipio = document.getElementById("municipio").value
-    
+    let requerimento = document.getElementById("requerimento").value
+    let ato = document.getElementById("Ato").value
+    let atividade = "Pecuária";
+    let porte = document.getElementById("Porte").value
+    let propriedade = document.getElementById("areaProp").value
+    let areaAtividade = document.getElementById("areaAtvd").value
+    let endereco = document.getElementById("Endereço").value
 
-    localStorage.setItem("codigoProcesso", cod); //setta os valores
-    localStorage.setItem("nomeRequerente", nome);
-    localStorage.setItem("nomeMunicipio", municipio + " - TO ");
+
+    salvar("codigoProcesso", cod); //setta os valores
+    salvar("nomeRequerente", nome);
+    salvar("nomeMunicipio", municipio + " - TO ");
+    salvar("requerimento", requerimento);
+    salvar("ato", ato);
+    salvar("atividade", atividade);
+    salvar("porte", porte);
+    salvar("Prop", propriedade);
+    salvar("areaAtvd", areaAtividade);
+    salvar("Endereço", endereco)
 
 
     window.location.href = "documento.html"; //redireciona pra proxima pagina
