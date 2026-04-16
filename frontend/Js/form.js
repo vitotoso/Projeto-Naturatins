f7f2b0e9ff262804b964c4f2b59e0c156b33517a
Daniel <akirazcs@gmail.com>
2026-04-16 16:58:19 -0300

    testes de imagem

--- frontend/Js/form.js
+++ frontend/Js/form.js
@@ -2,9 +2,7 @@ const form = document.querySelector("form");
 
 form.addEventListener("submit", function(event){ // praticamente quando o formulario for enviado vai executar a função
     event.preventDefault(); // impede a paginad e recarregar e perder o processo
-
     const salvar = (chave, valor) => localStorage.setItem(chave, valor)
-
     let cod = document.getElementById("cod_processo").value;
     let nome = document.getElementById("nome_requerente").value;
     // let estado = document.getElementById("estado").value;
@@ -16,10 +14,7 @@ form.addEventListener("submit", function(event){ // praticamente quando o formul
     let propriedade = document.getElementById("areaProp").value
     let areaAtividade = document.getElementById("areaAtvd").value
     let endereco = document.getElementById("Endereço").value
-
     const verificar = (areaAtividade,propriedade) => {"console"}
-
-
     salvar("codigoProcesso", cod); //setta os valores
     salvar("nomeRequerente", nome);
     salvar("nomeMunicipio", municipio + " - TO ");
@@ -31,6 +26,10 @@ form.addEventListener("submit", function(event){ // praticamente quando o formul
     salvar("areaAtvd", areaAtividade);
     salvar("Endereço", endereco)
 
+
+
+
+    const checkboxes = document.getElementsByName("grupo")
 
     window.location.href = "documento.html"; //redireciona pra proxima pagina
 });
