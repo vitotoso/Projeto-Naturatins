c8aeb1896b606a124388bb70731ada8ab1ed2861
Daniel <akirazcs@gmail.com>
2026-04-29 19:46:56 -0300

    "merge" de ambiente-de-testes

--- frontend/Js/doc.js
+++ frontend/Js/doc.js
@@ -1,29 +1,33 @@
-let cod = localStorage.getItem("cod_processo");
-let car = localStorage.getItem("cod_car");
-let nome = localStorage.getItem("nome_requerente");
-let req = localStorage.getItem("requerimento");
-let ato = localStorage.getItem("Ato");
-let atvd = localStorage.getItem("atividade");
-let porte = localStorage.getItem("Porte");
-let areaProp = localStorage.getItem("areaProp");
-let areaAtvd = localStorage.getItem("areaAtvd");
-let cid = localStorage.getItem("municipio");
-let endereco = localStorage.getItem("Endereco");
-let dataChegada = localStorage.getItem("dataChegada");
-let Obs = localStorage.getItem("Obs");
-let Cond = localStorage.getItem("Cond");
-let OpcDoc = localStorage.getItem("OpcDoc");
-let OpcCar = localStorage.getItem("OpcCar");
-let OpcArt = localStorage.getItem("OpcArt");
-let OpcAgua = localStorage.getItem("OpcAgua");
-let OpcApoio = localStorage.getItem("OpcApoio");
-let OpcSpr = localStorage.getItem("OpcSpr");
-let OpcInfr = localStorage.getItem("OpcInfr");
-let OpcResi = localStorage.getItem("OpcResi");
-let OpcCons = localStorage.getItem("OpcCons");
-let OpcAna = localStorage.getItem("OpcAna");
+const inputs = {
+  cod: "codProc",
+  car: "codCar",
+  nome: "nomeReq",
+  req: "requerimento",
+  ato: "ato",
+  atvd: "atividade",
+  porte: "porte",
+  areaProp: "areaProp",
+  areaAtvd: "areaAtvd",
+  cid: "municipio",
+  endereco: "endereco",
+  dataChegada: "dataChegada",
+  Obs: "obs",
+  Cond: "cond",
+  RespTec: "respTec",
+  OpcDoc: "opcDoc",
+  OpcCar: "opcCar",
+  OpcArt: "opcArt",
+  OpcAgua: "opcAgua",
+  OpcApoio: "opcApoio",
+  OpcSpr: "opcSpr",
+  OpcInfr: "opcInfr",
+  OpcResi: "opcResi",
+  OpcCons: "opcCons",
+  OpcAna: "opcAna"
+};
 
-console.log();
+
+
 
 const mostrar = (id, valor) => {
   //função pra pegar todos os elementos que vai ter o id ali, e substituir o texto por valor, e a lista retornar 0 no lugar ele coloca ??
@@ -35,31 +39,17 @@ const mostrar = (id, valor) => {
     elemento.textContent = valor ?? "";
   });
 };
+const comHa = ["areaProp", "areaAtvd"];
 
-mostrar("cod", cod);
-mostrar("car", car);
-mostrar("req", req);
-mostrar("nome", nome);
-mostrar("ato", ato);
-mostrar("porte", porte);
-mostrar("atvd", atvd);
-mostrar("areaProp", areaProp);
-mostrar("areaAtvd", areaAtvd);
-mostrar("cid", cid);
-mostrar("endereco", endereco);
-mostrar("dataChegada", dataChegada);
-mostrar("Obs", Obs);
-mostrar("Cond", Cond);
-mostrar("OpcDoc", OpcDoc);
-mostrar("OpcCar", OpcCar);
-mostrar("OpcArt", OpcArt);
-mostrar("OpcAgua", OpcAgua);
-mostrar("OpcApoio", OpcApoio);
-mostrar("OpcSpr", OpcSpr);
-mostrar("OpcInfr", OpcInfr);
-mostrar("OpcResi", OpcResi);
-mostrar("OpcCons", OpcCons);
-mostrar("OpcAna", OpcAna);
+Object.entries(inputs).forEach(([campo, chave]) => {
+  let valor = localStorage.getItem(chave)
+
+  if (comHa.includes(chave)) {
+    valor = (valor ?? "") + " ha";
+  }
+  mostrar(campo, valor);
+});
+
 
 const data = () => {
   const hoje = new Date();
